9068c109a5455546db1530d0d74b51747fe4a65b
shekar mudaliyar <sskshekar10@gmail.com>
2019-07-25 15:50:08 +0530

    change in plan

--- browser_automation.py
+++ browser_automation.py
@@ -7,9 +7,9 @@ from selenium.webdriver.common.keys import Keys
 firefox_profile = webdriver.FirefoxProfile()
 firefox_profile.set_preference('permissions.default.image', 2)
 firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
-driver = webdriver.Firefox(firefox_profile=firefox_profile)
+# driver = webdriver.Firefox(firefox_profile=firefox_profile)
 driver_new = webdriver.Firefox(firefox_profile=firefox_profile)
-driver.get("https://www.inshorts.com/en/read")
+# driver.get("https://www.inshorts.com/en/read")
 
 def button_click(number,time):
     for i in range(number):
@@ -66,7 +66,10 @@ def get_from_timesnow(link,index,news_title,news_summary,source_name):
 def get_from_hindustantimes(link,index,news_title,news_summary,source_name):
     driver_new.get(link)
     time.sleep(5)
-    driver_new.find_element_by_class_name('btn_disagree').click()
+    try:
+        driver_new.find_element_by_class_name('btn_disagree').click()
+    except:
+        print('hmm')    
     text = driver_new.find_element_by_class_name('story-details').find_elements_by_css_selector('p')
     l = []
     for i in range(len(text)):
@@ -460,99 +463,98 @@ def get_from_spotboye(link,index,news_title,news_summary,source_name):
 def others():
     print('Null')
 
-def get_from_source(link,index,news_title,news_summary):
-    print(link.get_attribute('href'))
-    print(link.text)
-    if(link.text=='TechCrunch'||link.text=='Tech Crunch'):
-        get_from_techCrunch(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Livemint'):
-        get_from_livemint(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='FTC'):
-        get_from_ftc(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Delhi Planet'):
-        get_from_delhiplanet(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Phys.org'):
-        get_from_phys(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='PRI'):
-        get_from_pri(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='BGR'||link.text=='BGR India'):
-        get_from_bgr(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='PNAS'):
-        get_from_pnas(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='TechCircle'):
-        get_from_techcircle(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Purdue University'):
-        get_from_purdue(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Times Now'):
-        get_from_timesnow(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Hindustan Times'):
-        get_from_hindustantimes(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='The New Indian Express'):
-        get_from_newindianexpress(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='The Guardian'):
-        get_from_guardian(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='CricTracker'):
-        get_from_crictracker(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='India.com'):
-        get_from_indiacom(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Daily Mail'):
-        get_from_dailymail(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='The News Minute'):
-        get_from_thenewsminute(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='ANI'||link.text=='ANI News'):
-        if link.get_attribute('href').find("twitter.com") == -1:
-            get_from_ani(link.get_attribute('href'),index,news_title,news_summary,link.text)
+def get_from_source(link,index,news_title,news_summary,link_name):
+    print(link)
+    print(link_name)
+    if(link_name=='TechCrunch' or link_name=='Tech Crunch'):
+        get_from_techCrunch(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Livemint'):
+        get_from_livemint(link,index,news_title,news_summary,link_name)
+    elif(link_name=='FTC'):
+        get_from_ftc(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Delhi Planet'):
+        get_from_delhiplanet(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Phys.org'):
+        get_from_phys(link,index,news_title,news_summary,link_name)
+    elif(link_name=='PRI'):
+        get_from_pri(link,index,news_title,news_summary,link_name)
+    elif(link_name=='BGR' or link_name=='BGR India'):
+        get_from_bgr(link,index,news_title,news_summary,link_name)
+    elif(link_name=='PNAS'):
+        get_from_pnas(link,index,news_title,news_summary,link_name)
+    elif(link_name=='TechCircle'):
+        get_from_techcircle(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Purdue University'):
+        get_from_purdue(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Times Now'):
+        get_from_timesnow(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Hindustan Times'):
+        get_from_hindustantimes(link,index,news_title,news_summary,link_name)
+    elif(link_name=='The New Indian Express'):
+        get_from_newindianexpress(link,index,news_title,news_summary,link_name)
+    elif(link_name=='The Guardian'):
+        get_from_guardian(link,index,news_title,news_summary,link_name)
+    elif(link_name=='CricTracker'):
+        get_from_crictracker(link,index,news_title,news_summary,link_name)
+    elif(link_name=='India.com'):
+        get_from_indiacom(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Daily Mail'):
+        get_from_dailymail(link,index,news_title,news_summary,link_name)
+    elif(link_name=='The News Minute'):
+        get_from_thenewsminute(link,index,news_title,news_summary,link_name)
+    elif(link_name=='ANI' or link_name=='ANI News'):
+        if link.find("twitter.com") == -1:
+            get_from_ani(link,index,news_title,news_summary,link_name)
         else:
             others()
-    elif(link.text=='The Quint'):
-        get_from_quint(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Bloomberg Quint'||link.text=='BloombergQuint'):
-        get_from_bloombergquint(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Inc42'):
-        get_from_inc42(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Reuters'):
-        get_from_reuters(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='The Financial Express'||link.text=='Financial Express'):
-        get_from_financialexpress(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='RT'):
-        get_from_rt(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Free Press Journal'||link.text=='The Free Press Journal'):
-        get_from_freepressjournal(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='The Wire'):
-        get_from_thewire(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Engadget'):
-        get_from_engadget(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Vogue India'):
-        get_from_vogueindia(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Entrackr'):
-        get_from_entrackr(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='SpotboyE'||link.text=='SpotBoyE'):
-        get_from_spotboye(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='PTI'):
-        get_from_pti(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Bollywood Hungama'):
-        get_from_bollywoodhungama(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Pinkvilla'):
-        get_from_pinkvilla(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Cricket Country'):
-        get_from_cricketcountry(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='The White House'):
-        get_from_whitehouse(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='YourStory'):
-        get_from_yourstory(link.get_attribute('href'),index,news_title,news_summary,link.text)
-    elif(link.text=='Cricket Australia'):
-        get_from_cricketaustralia(link.get_attribute('href'),index,news_title,news_summary,link.text)
+    elif(link_name=='The Quint'):
+        get_from_quint(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Bloomberg Quint' or link_name=='BloombergQuint'):
+        get_from_bloombergquint(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Inc42'):
+        get_from_inc42(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Reuters'):
+        get_from_reuters(link,index,news_title,news_summary,link_name)
+    elif(link_name=='The Financial Express' or link_name=='Financial Express'):
+        get_from_financialexpress(link,index,news_title,news_summary,link_name)
+    elif(link_name=='RT'):
+        get_from_rt(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Free Press Journal' or link_name=='The Free Press Journal'):
+        get_from_freepressjournal(link,index,news_title,news_summary,link_name)
+    elif(link_name=='The Wire'):
+        get_from_thewire(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Engadget'):
+        get_from_engadget(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Vogue India'):
+        get_from_vogueindia(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Entrackr'):
+        get_from_entrackr(link,index,news_title,news_summary,link_name)
+    elif(link_name=='SpotboyE' or link_name=='SpotBoyE'):
+        get_from_spotboye(link,index,news_title,news_summary,link_name)
+    elif(link_name=='PTI'):
+        get_from_pti(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Bollywood Hungama'):
+        get_from_bollywoodhungama(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Pinkvilla'):
+        get_from_pinkvilla(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Cricket Country'):
+        get_from_cricketcountry(link,index,news_title,news_summary,link_name)
+    elif(link_name=='The White House'):
+        get_from_whitehouse(link,index,news_title,news_summary,link_name)
+    elif(link_name=='YourStory'):
+        get_from_yourstory(link,index,news_title,news_summary,link_name)
+    elif(link_name=='Cricket Australia'):
+        get_from_cricketaustralia(link,index,news_title,news_summary,link_name)
     else:
         others()
 
 
 def get_from_inshots():
-    button_click(500,10)
+    button_click(1000,10)
     news = driver.find_elements_by_class_name('news-card')
     for i in range(len(news)):
         print(i)
-        news_summary_title = news[i].find_element_by_class_name(
-            'news-card-title').find_element_by_css_selector('a').text
+        news_summary_title = news[i].find_element_by_class_name('news-card-title').find_element_by_css_selector('a').text
         print(news_summary_title)
         news_summary = news[i].find_element_by_class_name('news-card-content').text
         print(news_summary)
@@ -564,7 +566,17 @@ def get_from_inshots():
     driver.quit()
     driver_new.quit()
 
-get_from_inshots()
+def get_from_csv():
+    with open('documentsummaries.csv', 'r') as f:
+          reader = csv.reader(f)
+          your_list = list(reader)
+          for i in range(len(your_list)):
+              get_from_source(your_list[i][4],your_list[i][0],your_list[i][1],your_list[i][2],your_list[i][3])
+    print(your_list)
+
+
+get_from_csv()
+# get_from_inshots()
 # get_from_livemint('.')
 # sumTitle = driver.find_elements_by_xpath('//*[@itemprop="headline"]')
 # sumArticle = driver.find_elements_by_xpath(
